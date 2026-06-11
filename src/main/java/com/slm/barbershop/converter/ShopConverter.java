6eceeafd5f9ce0dcf49117934bc02ba7766bf934
zhangcb <15867153769@163.com>
2026-06-11 12:01:42 +0800

    feat(ui): 店铺管理默认概览信息为展示修复

--- src/main/java/com/slm/barbershop/converter/ShopConverter.java
+++ src/main/java/com/slm/barbershop/converter/ShopConverter.java
@@ -3,17 +3,14 @@ package com.slm.barbershop.converter;
 import com.slm.barbershop.entity.Shop;
 import com.slm.barbershop.model.ShopRequest;
 import com.slm.barbershop.model.ShopResponse;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.ReportingPolicy;
-import org.mapstruct.TargetType;
+import org.mapstruct.*;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ShopConverter {
 
-    void updateEntity(@TargetType Shop shop, ShopRequest request);
+    void updateEntity(@MappingTarget Shop shop, ShopRequest request);
 
     @Mapping(target = "userId", source = "userId")
     Shop toEntityWithUserId(ShopRequest request, Long userId);
