5f785f94eccfc5c930bd6f07d8165367b3fada6b
zhangcb <15867153769@163.com>
2026-06-24 18:00:12 +0800

    feat(member): 新增跨店铺会员匹配查询

--- src/main/java/com/slm/barbershop/mapper/MemberMapper.java
+++ src/main/java/com/slm/barbershop/mapper/MemberMapper.java
@@ -2,9 +2,15 @@ package com.slm.barbershop.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.slm.barbershop.entity.Member;
+import com.slm.barbershop.model.MemberMatchVO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface MemberMapper extends BaseMapper<Member> {
+
+    List<MemberMatchVO> matchAcrossShops(@Param("phone") String phone, @Param("name") String name);
 
 }
